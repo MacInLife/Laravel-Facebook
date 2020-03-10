d343ad8a86592e2c143b1bfe405fc708c0a11704
MacInLife <marie.ange.coco@gmail.com>
2020-03-10 18:35:25 +0100

    Modif page welcome

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Facebook Laravel
 
-Application de page unique de type Facebook avec Laravel.
+Créer un réseau social clone de Facebook en utilisant le framework PHP Laravel.
 
 ## I - Création du projet
 
@@ -91,11 +91,233 @@ Si dans votre BDD, vous pouvez voir les utilisateurs crée, c'est que la connexi
 
 Gestionnaire de version de notre projet, pour garantir l'optimisation et la sauvegarde du projet.
 
-1. Crée un nouveau référentiel sur la ligne de commande
-   `git init`<br>
+1. Crée un nouveau référentiel sur la ligne de commande :
 
-2. Pousser un référentiel existant depuis la ligne de commande
-   `git remote add origin https://lien_de_votre_projet.git`<br>
-   `git push -u origin master`<br>
+    ```
+    git init
+    ```
+
+2. Pousser un référentiel existant depuis la ligne de commande :
+
+```
+   git remote add origin https://lien_de_votre_projet.git
+   git push -u origin master
+```
 
 3. Faire un premier commit
+4. Push son projet.
+
+## III - Modification de la page d'accueil
+
+Transformation de la page d'accueil de LARAVEL en une page d'accueil similaire à celle de Facebook.
+A. Pour cela, il faut modifier le fichier **"welcome.blade.php"**
+Il se situe dans le dossier ./ressources/views
+
+<details>
+<summary>
+Code de la page welcome.blade.php
+</summary>
+
+```php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Facebook</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .left,
+        .right {
+            width: 50%;
+            justify-content: center;
+        }
+
+    </style>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background:#385898;">
+        <div class="container">
+            <a style="font-size:1.5rem;" class="navbar-brand text-white" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}<img class="pl-2 mb-1" src="img/facebook.png" alt="" width="120">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    <form method="POST" action="{{ route('login') }}" class="d-flex">
+                        @csrf
+
+                        <div class="form-group px-2 m-0">
+                            <label style="font-size:12px;" for="email"
+                                class="text-md-right text-white mb-0">{{ __('Adresse e-mail') }}</label>
+
+                            <div class="">
+                                <input style="font-size:12px;" id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group px-2 m-0">
+                            <label style="font-size:12px;" for="password"
+                                class="text-md-right text-white mb-0">{{ __('Mot de passe') }}</label>
+
+                            <div class="">
+                                <input style="font-size:12px;" id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                                @if (Route::has('password.request'))
+                                <a style="color:#9cb4d8; font-size:12px;" class="btn btn-link"
+                                    href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group m-auto">
+                            <div class="offset-md-2 pb-1">
+                                <button style="font-size:12px;" type="submit" class="btn btn-primary">
+                                    {{ __('Connexion') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container d-flex flex-nowrap">
+        <div class="left m-2">
+            <H1
+                style="color:#0e385f; font-size: 20px; font-weight: bold; line-height: 29px;
+    margin-top: 40px; width: 450px; word-spacing: -1px; font-family: system-ui, -apple-system, BlinkMacSystemFont, '.SFNSText-Regular', sans-serif;">
+                Avec Facebook, partagez et restez en contact avec votre entourage</H1>
+            <div>
+                <img src="img/ObaVg52wtTZ.png" alt="" width="437">
+            </div>
+        </div>
+        <div class="row right d-flex justify-content-center m-2 ">
+
+            <H1 class="row"
+                style="  margin-top: 40px; font-family: system-ui, -apple-system, BlinkMacSystemFont, '.SFNSText-Regular', sans-serif;">
+                {{ __('Inscription') }}</H1>
+
+            <div class="col-md-10">
+                <p>C’est rapide et facile.</p>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="d-flex justify-content-between">
+                        <!-- Champ Prénom -->
+                        <div class="form-group row mr-5">
+                            <input id="firstname" type="text"
+                                class="form-control @error('firstname') is-invalid @enderror" name="firstname"
+                                value="{{ old('firstname') }}" placeholder="Prénom" required autocomplete="firstname"
+                                autofocus>
+
+                            @error('firstname')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <!-- Champ Nom -->
+                        <div class="form-group row">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                name="name" value="{{ old('name') }}" placeholder="Nom de famille" required
+                                autocomplete="name" autofocus>
+
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Champ Mail -->
+                    <div class="form-group row">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" placeholder="Adresse e-mail" required
+                            autocomplete="email">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <!-- Champ Mot de passe -->
+                    <div class="form-group row">
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password"
+                            placeholder="Mot de passe" required autocomplete="new-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <!-- Champ Mot de passe de confirmation -->
+                    <div class="form-group row">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                            placeholder="Mot de passe de confirmation" required autocomplete="new-password">
+                    </div>
+
+                    <!-- Bouton d'envoi du formulaire d'inscription -->
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary btn-success">
+                                {{ __('Inscription') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+```
+
+</details>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,197 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel Facebook</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .left,
+        .right {
+            width: 50%;
+            justify-content: center;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    </style>
+</head>
 
-            .position-ref {
-                position: relative;
-            }
+<body>
+    <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background:#385898;">
+        <div class="container">
+            <a style="font-size:1.5rem;" class="navbar-brand text-white" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}<img class="pl-2 mb-1" src="img/facebook.png" alt="" width="120">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-            .content {
-                text-align: center;
-            }
+                </ul>
 
-            .title {
-                font-size: 84px;
-            }
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    <form method="POST" action="{{ route('login') }}" class="d-flex">
+                        @csrf
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+                        <div class="form-group px-2 m-0">
+                            <label style="font-size:12px;" for="email"
+                                class="text-md-right text-white mb-0">{{ __('Adresse e-mail') }}</label>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+                            <div class="">
+                                <input style="font-size:12px;" id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                        <div class="form-group px-2 m-0">
+                            <label style="font-size:12px;" for="password"
+                                class="text-md-right text-white mb-0">{{ __('Mot de passe') }}</label>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                            <div class="">
+                                <input style="font-size:12px;" id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                                @if (Route::has('password.request'))
+                                <a style="color:#9cb4d8; font-size:12px;" class="btn btn-link"
+                                    href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group m-auto">
+                            <div class="offset-md-2 pb-1">
+                                <button style="font-size:12px;" type="submit" class="btn btn-primary">
+                                    {{ __('Connexion') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                </ul>
             </div>
         </div>
-    </body>
+    </nav>
+
+    <div class="container d-flex flex-nowrap">
+        <div class="left m-2">
+            <H1
+                style="color:#0e385f; font-size: 20px; font-weight: bold; line-height: 29px;
+    margin-top: 40px; width: 450px; word-spacing: -1px; font-family: system-ui, -apple-system, BlinkMacSystemFont, '.SFNSText-Regular', sans-serif;">
+                Avec Facebook, partagez et restez en contact avec votre entourage</H1>
+            <div>
+                <img src="img/ObaVg52wtTZ.png" alt="" width="437">
+            </div>
+        </div>
+        <div class="row right d-flex justify-content-center m-2 ">
+
+            <H1 class="row"
+                style="  margin-top: 40px; font-family: system-ui, -apple-system, BlinkMacSystemFont, '.SFNSText-Regular', sans-serif;">
+                {{ __('Inscription') }}</H1>
+
+            <div class="col-md-10">
+                <p>C’est rapide et facile.</p>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="d-flex justify-content-between">
+                        <!-- Champ Prénom -->
+                        <div class="form-group row mr-5">
+                            <input id="firstname" type="text"
+                                class="form-control @error('firstname') is-invalid @enderror" name="firstname"
+                                value="{{ old('firstname') }}" placeholder="Prénom" required autocomplete="firstname"
+                                autofocus>
+
+                            @error('firstname')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <!-- Champ Nom -->
+                        <div class="form-group row">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                name="name" value="{{ old('name') }}" placeholder="Nom de famille" required
+                                autocomplete="name" autofocus>
+
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Champ Mail -->
+                    <div class="form-group row">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" placeholder="Adresse e-mail" required
+                            autocomplete="email">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <!-- Champ Mot de passe -->
+                    <div class="form-group row">
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password"
+                            placeholder="Mot de passe" required autocomplete="new-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <!-- Champ Mot de passe de confirmation -->
+                    <div class="form-group row">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                            placeholder="Mot de passe de confirmation" required autocomplete="new-password">
+                    </div>
+
+                    <!-- Bouton d'envoi du formulaire d'inscription -->
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary btn-success">
+                                {{ __('Inscription') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
