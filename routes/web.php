36b8b715ceb239c97bed479a1ca4e46614b9f19a
MacInLife <marie.ange.coco@gmail.com>
2020-04-17 16:29:04 +0200

    composer.lock delete

--- routes/web.php
+++ routes/web.php
@@ -24,26 +24,26 @@ Auth::routes();
 Route::get('/search', 'SearchController@index')->name('search');
 //Route de vision de la page de fil d'actualité
 Route::get('/home', 'PostController@index')->name('home');
-//Route de la méthode publier un post (création)
-// Route::post('/home', 'PostController@create')->middleware('auth')->name('create.post');
-// //Route de la méthode répondre à un post par un commentaire (création)
-// Route::post('/', 'ComController@createCom')->middleware('auth')->name('createCom.com');
-// //Route de la méthode delete un post (suppression)
-// Route::get('/home/{id}', 'PostController@destroy')->middleware('auth')->name('destroy.post');
-// //Route de la méthode delete un post (suppression)
-// Route::get('/com/{id}', 'ComController@destroyCom')->middleware('auth')->name('destroyCom.com');
-// Route::get('/home/{id}/like', 'PostController@like')->name('post.like');
-// Route::get('/home/{id}/unlike', 'PostController@unlike')->name('post.unlike');
+Route de la méthode publier un post (création)
+Route::post('/home', 'PostController@create')->middleware('auth')->name('create.post');
+//Route de la méthode répondre à un post par un commentaire (création)
+Route::post('/', 'ComController@createCom')->middleware('auth')->name('createCom.com');
+//Route de la méthode delete un post (suppression)
+Route::get('/home/{id}', 'PostController@destroy')->middleware('auth')->name('destroy.post');
+//Route de la méthode delete un post (suppression)
+Route::get('/com/{id}', 'ComController@destroyCom')->middleware('auth')->name('destroyCom.com');
+Route::get('/home/{id}/like', 'PostController@like')->name('post.like');
+Route::get('/home/{id}/unlike', 'PostController@unlike')->name('post.unlike');
 
-// Route::get('account', 'AccountController@show')->middleware('auth')->name('account');
-// Route::post('account/{id}', 'AccountController@update')->middleware('auth')->name('account.update');
-// Route::post('/account', 'AccountController@destroyAvatar')->middleware('auth')->name('account.destroyAvatar');
-// Route::get('/account/{id}', 'AccountController@destroy')->middleware('auth')->name('account.destroy');
+Route::get('account', 'AccountController@show')->middleware('auth')->name('account');
+Route::post('account/{id}', 'AccountController@update')->middleware('auth')->name('account.update');
+Route::post('/account', 'AccountController@destroyAvatar')->middleware('auth')->name('account.destroyAvatar');
+Route::get('/account/{id}', 'AccountController@destroy')->middleware('auth')->name('account.destroy');
 
 
-// Route::get('/profil/{slug}', 'ProfilController@index')->name('profil');
-// Route::post('profil/{slug}', 'ProfilController@updateAvatar')->middleware('auth')->name('profil.updateAvatar');
-// Route::post('profil', 'ProfilController@updateCover')->middleware('auth')->name('profil.updateCover');
-// Route::get('/profil/{slug}/amis_add', 'ProfilController@amis_add')->name('profil.amisAdd');
-// Route::get('/profil/{slug}/amis_invit', 'ProfilController@amis_invit')->name('profil.amisInvit');
-// Route::get('/profil/{slug}/amis_delete', 'ProfilController@amis_delete')->name('profil.amisDelete');
+Route::get('/profil/{slug}', 'ProfilController@index')->name('profil');
+Route::post('profil/{slug}', 'ProfilController@updateAvatar')->middleware('auth')->name('profil.updateAvatar');
+Route::post('profil', 'ProfilController@updateCover')->middleware('auth')->name('profil.updateCover');
+Route::get('/profil/{slug}/amis_add', 'ProfilController@amis_add')->name('profil.amisAdd');
+Route::get('/profil/{slug}/amis_invit', 'ProfilController@amis_invit')->name('profil.amisInvit');
+Route::get('/profil/{slug}/amis_delete', 'ProfilController@amis_delete')->name('profil.amisDelete');
